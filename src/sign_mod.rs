c6e8aa058360b5807c8f7cf8f451daae556381aa
Nayv <43297329+nayvcake@users.noreply.github.com>
2022-03-05 18:27:15 -0300

    aaaaaa

--- src/sign_mod.rs
+++ src/sign_mod.rs
@@ -30,19 +30,19 @@ use self::ed25519_dalek::ed25519::signature::Signature;
 pub fn verify_authorization(pub_key: String, sign: String, message: String) -> bool {
     let public_key = hex::decode(pub_key);
     if public_key.is_err() {
-        eprintln!(hex_signature.err());
+        eprintln!("err -> Err: {}", hex_signature.err());
         return false; }
     let hex_signature = hex::decode(sign);
     if hex_signature.is_err() {
-        eprintln!(hex_signature.err());
+        eprintln!("err -> Err: {}", hex_signature.err());
         return false; }
     let public_key = PublicKey::from_bytes(&public_key.unwrap());
     if public_key.is_err() {
-        eprintln!(hex_signature.err());
+        eprintln!("err -> Err: {}", hex_signature.err());
         return false; }
     let signature = ed25519_dalek::Signature::from_bytes(&hex_signature.unwrap());
     if signature.is_err() {
-        eprintln!(hex_signature.err());
+        eprintln!("err -> Err: {}", hex_signature.err());
         return false; }
     return public_key.unwrap().verify(message.as_bytes(), &signature.unwrap()).is_ok()
 }
