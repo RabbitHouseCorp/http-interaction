981ecad6cd898c36043df41b31197f077f1f9fe1
Nayv <43297329+nayvcake@users.noreply.github.com>
2022-03-05 22:25:12 -0300

    Err, Discord?

--- src/routes/interaction/interaction_create.rs
+++ src/routes/interaction/interaction_create.rs
@@ -15,7 +15,7 @@ use crate::routes::websocket::websocket_server::convert_to_binary;
 // WithStatus<Json>
 pub async fn interaction_create(sign: String, timestamp: String, json: HashMap<String, Value>, clients: Clients) -> Result<impl warp::Reply, Infallible>
 {
-    let verify_sign = sign_mod::verify_authorization(String::from(""), sign, format!("{}{}", timestamp, json!(json)));
+    let verify_sign = sign_mod::verify_authorization(String::from("bd3116925977b042fd3a3775cfaa40231fe8a767c167c2d48b1f9ca56181b8a1"), sign, format!("{}{}", timestamp, json!(json)));
     match verify_sign {
         true => {
             let type_interaction: &Value = json.get("type").unwrap();
@@ -32,12 +32,16 @@ pub async fn interaction_create(sign: String, timestamp: String, json: HashMap<S
                         if let Err(_disconnected) =  client.tx.send(Message::binary(convert_to_binary(&json!(json)))) {
 
                                return Ok(warp::reply::with_status(warp::reply::json(&json!({ "type": 4, "data": {
+                                "tts": false,
+								"content": "There was a problem with the interaction!",
                                 "embeds": [
                                     {
+                                        "color":       "#ff1212",
                                         "description": "The connection was lost by the bot. Contact bot developer."
                                     }
-                                ]
-                            } }).as_object_mut()), warp::http::StatusCode::INTERNAL_SERVER_ERROR));
+                                ],
+                                   "allowed_mentions": []
+                            } }).as_object_mut()), warp::http::StatusCode::OK));
 
                             break;
                         }
@@ -49,17 +53,21 @@ pub async fn interaction_create(sign: String, timestamp: String, json: HashMap<S
                 eprintln!("Application offline");
 
                 return Ok(warp::reply::with_status(warp::reply::json(&json!({ "type": 4, "data": {
-                        "embeds": [
-                            {
-                                "description": "Application is offline! Try again later!"
-                            }
-                        ]
-                    } }).as_object_mut()), warp::http::StatusCode::INTERNAL_SERVER_ERROR))
+                                "tts": false,
+								"content": "There was a problem with the interaction!",
+                                "embeds": [
+                                    {
+                                        "color":       "#ff1212",
+                                        "description": "I didn't get a response from the bot, try again or you can contact the developer through the support server."
+                                    }
+                                ],
+                                "allowed_mentions": []
+                    } }).as_object_mut()), warp::http::StatusCode::OK))
 
             }
 
             Ok(
-                warp::reply::with_status(warp::reply::json(&json!({ "status_code": 200, "message": "Interaction unknown or not recognized", "error": false, "code_error": "HTTP_INTERACTION_UNKNOWN" }).as_object_mut()), warp::http::StatusCode::OK)
+                warp::reply::with_status(warp::reply::json(&json!({ "status_code": 200, "message": "Interaction unknown or not recognized", "error": false, "code_error": "HTTP_INTERACTION_UNKNOWN" }).as_object_mut()), warp::http::StatusCode::INTERNAL_SERVER_ERROR)
             )
         }
         false => {
