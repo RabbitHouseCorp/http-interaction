6bb6a7ebfb0ac38868f916c392f010e74bf84ca8
Nayv <43297329+nayvcake@users.noreply.github.com>
2022-03-05 22:04:26 -0300

    Add future callback.

--- src/routes/interaction/interaction_create.rs
+++ src/routes/interaction/interaction_create.rs
@@ -21,39 +21,41 @@ pub async fn interaction_create(sign: String, timestamp: String, json: HashMap<S
             let type_interaction: &Value = json.get("type").unwrap();
             let type_int = type_interaction.as_u64().unwrap();
             if type_int == HTTP_INTERACTION_CONFIRMATION_BOT {
-                warp::reply::with_status(warp::reply::json(&json!({ "type": 1 }).as_object_mut()), warp::http::StatusCode::OK);
+                return Ok(warp::reply::with_status(warp::reply::json(&json!({ "type": 1 }).as_object_mut()), warp::http::StatusCode::OK));
             }
             if type_int == INTERACTION_COMMAND {
                 if json.get("application_id").is_none() {
-                    warp::reply::with_status(warp::reply::json(&json!({ "type": 5, "message_err": "API cannot accept this metadata because application was not included! Please resend again." }).as_object_mut()), warp::http::StatusCode::NOT_ACCEPTABLE);
+                    return Ok(warp::reply::with_status(warp::reply::json(&json!({ "type": 5, "message_err": "API cannot accept this metadata because application was not included! Please resend again." }).as_object_mut()), warp::http::StatusCode::NOT_ACCEPTABLE))
                 }
                 for (id, client) in clients.read().await.iter() {
                     if json.get("application_id").unwrap() == id {
                         if let Err(_disconnected) =  client.tx.send(Message::binary(convert_to_binary(&json!(json)))) {
 
-                                warp::reply::with_status(warp::reply::json(&json!({ "type": 4, "data": {
+                               return Ok(warp::reply::with_status(warp::reply::json(&json!({ "type": 4, "data": {
                                 "embeds": [
                                     {
                                         "description": "The connection was lost by the bot. Contact bot developer."
                                     }
                                 ]
-                            } }).as_object_mut()), warp::http::StatusCode::INTERNAL_SERVER_ERROR);
+                            } }).as_object_mut()), warp::http::StatusCode::INTERNAL_SERVER_ERROR));
 
                             break;
                         }
-                        warp::reply::with_status(warp::reply::json(&json!({ "type": 5 }).as_object_mut()), warp::http::StatusCode::OK);
+                        return Ok(warp::reply::with_status(warp::reply::json(&json!({ "type": 5 }).as_object_mut()), warp::http::StatusCode::OK));
 
                     }
                 }
 
                 eprintln!("Application offline");
-                warp::reply::with_status(warp::reply::json(&json!({ "type": 4, "data": {
+
+                return Ok(warp::reply::with_status(warp::reply::json(&json!({ "type": 4, "data": {
                         "embeds": [
                             {
                                 "description": "Application is offline! Try again later!"
                             }
                         ]
-                    } }).as_object_mut()), warp::http::StatusCode::INTERNAL_SERVER_ERROR);
+                    } }).as_object_mut()), warp::http::StatusCode::INTERNAL_SERVER_ERROR))
+
             }
 
             Ok(
